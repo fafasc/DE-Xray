f35702779f2feb937afe6587f9ec347ac9dd3a1e
fafasc <113488164+fafasc@users.noreply.github.com>
2023-04-08 02:21:05 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # set val
 python3 /app/self-ping.py &
-PORT=8080
+PORT=4433
 AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
 ParameterSSENCYPT=chacha20-ietf-poly1305
 CADDYIndexPage=https://www.libreoffice.org/download/download-libreoffice/
